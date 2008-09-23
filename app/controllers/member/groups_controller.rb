0b24a28fbea1631a66d82ef8315564cace2eb9c9
Aitor García <aitor.garcia@gmail.com>
2008-09-23 13:28:51 +0200

    Fix redirection when editing a group

--- app/controllers/member/groups_controller.rb
+++ app/controllers/member/groups_controller.rb
@@ -1,21 +1,21 @@
 class Member::GroupsController < Member::BaseController
-  
+
   before_filter :find_group, :except => [:index, :new, :create]
   before_filter :check_moderator, :except => [:index, :new, :create]
-     
+
   def index
     @moderator_memberships = current_user.moderator_memberships
     @plain_memberships = current_user.plain_memberships
   end
-     
+
   def create
     @group = Group.new(params[:group])
     @group.author = current_user
     @group.save
-    
+
     @group.join(current_user, true)
     @group.activate_membership(current_user)
-        
+
     if @group.errors.empty?
       if current_user.admin == true || Tog::Config['plugins.tog_social.group.moderation.creation'] != 'true'
          @group.activate!
@@ -24,22 +24,22 @@ class Member::GroupsController < Member::BaseController
       else
         GroupMailer.deliver_activation_request(@group)
         flash[:warning] = 'Your group has been created but is pending for administration approval.'
-        redirect_to groups_path          
-      end      
+        redirect_to groups_path
+      end
     else
       render :action => 'new'
-    end    
+    end
 
   end
-  
+
   def update
     @group.update_attributes!(params[:group])
     @group.tag_list = params[:group][:tag_list]
     @group.save
-    flash[:ok] = "Group #{@group.name} succcessfully updated!"     
-    redirect_to groups_show_path(@group)   
+    flash[:ok] = "Group #{@group.name} succcessfully updated!"
+    redirect_to group_path(@group)
   end
-  
+
   def reject_member
     user = User.find(params[:user_id])
     if !user
@@ -54,16 +54,16 @@ class Member::GroupsController < Member::BaseController
     else
       flash[:error] = "Ooops. something happened."
     end
-    redirect_to member_group_pending_members_url(@group)    
+    redirect_to member_group_pending_members_url(@group)
   end
-  
+
   def accept_member
     user = User.find(params[:user_id])
     if !user
       flash[:error] = "User doesn't exist"
       redirect_to pending_members_paths(@group)
       return
-    end    
+    end
     @group.activate_membership(user)
     if @group.members.include? user
         GroupMailer.deliver_accept_join_request(@group, current_user, user)
@@ -71,11 +71,11 @@ class Member::GroupsController < Member::BaseController
     else
       flash[:error] = "Ooops. something happened."
     end
-    redirect_to member_group_pending_members_url(@group) 
-  end  
-  
+    redirect_to member_group_pending_members_url(@group)
+  end
 
-  
+
+
   protected
 
 
@@ -88,6 +88,6 @@ class Member::GroupsController < Member::BaseController
       flash[:error] = "You are not one of this group's moderators."
       redirect_to groups_path(@group)
     end
-  end  
-  
+  end
+
 end
