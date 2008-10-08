1723e929bfab3ab7f9235a9edc8189dc1b294634
Roberto Salicio <roberto.salicio@gmail.com>
2008-10-08 10:53:06 +0200

    name method removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,20 @@
 class User < ActiveRecord::Base
 
-
   before_create :create_profile
 
   has_one :profile, :dependent => :destroy
   has_many :memberships
-  has_many :plain_memberships, :class_name => 'Membership', 
+  has_many :plain_memberships, :class_name => 'Membership',
                                :conditions => ['memberships.moderator <> ?', true]
-  has_many :moderator_memberships, :class_name => 'Membership', 
+  has_many :moderator_memberships, :class_name => 'Membership',
                                    :conditions => ['memberships.moderator = ?', true]
 
-  has_many :groups, :through => :memberships, 
+  has_many :groups, :through => :memberships,
                     :conditions => "memberships.state='active' and groups.state='active'"
 
-  has_many :moderated_groups, :through => :moderator_memberships, 
+  has_many :moderated_groups, :through => :moderator_memberships,
                     :conditions => "memberships.state='active' and groups.state='active'", :source => :group
 
-  def name
-      profile.full_name
-  end
-  
   protected
 
     def create_profile
