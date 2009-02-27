d17590cbd3521fd06f5d39fe36e51e04d7c3470b
Aitor García <aitor.garcia@gmail.com>
2009-02-27 11:08:38 +0100

    acts_as_scribe updated

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,6 +2,14 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < Test::Unit::TestCase
   context "A User" do
-    should_have_many :activities
+    should_have_one :profile
+    should_have_many :memberships
+    should_have_many :memberships
+    should_have_many :plain_memberships
+    should_have_many :moderator_memberships
+    should_have_many :groups
+    should_have_many :moderated_groups
+    should_have_many :client_applications
+    should_have_many :tokens
   end
 end
