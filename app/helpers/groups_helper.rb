654cbb74f7bf9dd4ad4090226ebe7b09d69a4de0
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-03-23 17:43:02 +0800

    moved share_ink to sharings_helper
    Signed-off-by: Aitor García Rey <aitor@linkingpaths.com>

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -33,11 +33,5 @@ module GroupsHelper
   def i_am_moderator_of(group)
     return group.moderators.include?(current_user)
   end
-  
-  def share_with_groups_link(shareable, only_moderated_groups=false)
-    return if !shareable
-    groups = only_moderated_groups ? current_user.moderated_groups : current_user.groups
-    render :partial => 'shared/share_with_groups', :locals => {:groups => groups, :shareable => shareable}
-  end
 
 end
--- app/helpers/sharings_helper.rb
+++ app/helpers/sharings_helper.rb
@@ -1,8 +1,14 @@
 module SharingsHelper
   
-  def share_link(text, shared, shareable)
-    link_to_remote text, :url => member_share_with_group_path(shared, shareable.type, shareable.id), 
-        :html => {:title => I18n.t("tog_social.sharing.share_with", :name => shared.name)} 
+  def share_with_groups_link(shareable, only_moderated_groups=false)
+    return if !shareable
+    groups = only_moderated_groups ? current_user.moderated_groups : current_user.groups
+    render :partial => 'shared/share_with_groups', :locals => {:groups => groups, :shareable => shareable}
+  end
+    
+  def share_link(text, share_with, shareable)
+    link_to_remote text, :url => member_share_with_group_path(share_with, shareable.type, shareable.id), 
+        :html => {:title => I18n.t("tog_social.sharing.share_with", :name => share_with.name)} 
   end
   
 end
