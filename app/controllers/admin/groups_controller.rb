01ae04daa3404f0e65f618af17b18f1ddd9fd7a9
Balint Erdi <balint.erdi@gmail.com>
2009-05-18 17:29:19 +0200

    Revert "admins can create new groups through the admin interface"
    This reverts commit 3a38377cf397e6b53e706500f4df824134f42529.
    
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -8,22 +8,6 @@ class Admin::GroupsController < Admin::BaseController
                              :order => "#{@order} ASC"
   end
 
-  def new
-    @group = Group.new
-  end
-  
-  def create
-    @group = Group.new(params[:group])
-    @group.author = current_user
-    if @group.save
-      flash[:ok] = I18n.t("tog_social.groups.message.creation_successful")
-      redirect_to admin_groups_path      
-    else
-      flash[:error] = I18n.t("tog_social.groups.message.creation_failed")
-      render :action => "new"
-    end
-  end
-  
   def edit
     @group = Group.find(params[:id])
   end
