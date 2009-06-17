c68eb266ccb08de1a32e50989d7c9b8bb430392c
Aitor García <aitor.garcia@gmail.com>
2009-06-17 13:56:45 +0200

    require should 2.10.1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environme
 require 'test_help'
 require 'test/unit'
 require 'mocha'
-require 'shoulda'
+
+begin
+  gem 'thoughtbot-shoulda', '>=2.10.1'
+  require 'shoulda' 
+rescue Exception => e
+  puts "\n\nYou need shoulda 2.10.1 or greater to test tog_core. Visit http://github.com/thoughtbot/shoulda to view details.\n\n" 
+  exit
+end
 
 require 'factory_girl'
 require File.expand_path(File.dirname(__FILE__) + '/factories')
